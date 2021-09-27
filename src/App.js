b258600a614873efe187fd05350cfdf0103990fe
elodiebhs <elodiebouthors@hotmail.com>
2021-09-27 12:20:21 -0700

    add table component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import Header from "./Components/Header";
+import Table from "./Components/Table"
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Table/>
     </div>
   );
 }
