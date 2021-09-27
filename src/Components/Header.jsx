5269ee494f76d19d8f3dfc9c8a4c512cd2ed83d2
elodiebhs <elodiebouthors@hotmail.com>
2021-09-27 11:38:09 -0700

    add content to navbar and header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import NavBar from './NavBar'
 
-export default function Header() {
-  
+import './Header.css';
+
+function Header () {
 
   return (
-    <section>
-      <NavBar/>
+    <section className="header">
+      <section className="header-top">
+        <section className="header-top__logo">
+          <a href="/" className="header-logo">LOGO</a>
+        </section>
+        <section className="header-top__navbar">
+          <section className="header-top__navigation">
+            <NavBar />
+          </section>
+          <hr className="header-top__seperator" />
+        </section>
+      </section>
+      <section className="header-bottom">
+        <section className="header-bottom__phone">
+          99999999999
+        </section>
+        <section className="header-bottom__email">
+          shop.info@gmail.com
+        </section>
+      </section>
     </section>
   )
-  };
+}
+
+export default Header;
