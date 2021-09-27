37e0c6a8866a91f5b75fc9e56ff7ff3287b885a0
elodiebhs <elodiebouthors@hotmail.com>
2021-09-27 13:36:48 -0700

    add info to userForm

--- src/Components/Form/UserFormParent.jsx
+++ src/Components/Form/UserFormParent.jsx
@@ -1,11 +1,72 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonnalDetails from './FormPersonnalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
 
 export default class UserFormParent extends Component {
+  state = {
+    step: 1,
+    firstName: '',
+    lastName: '',
+    email: '',
+    occupation: '',
+    city: '',
+    bio: ''
+  };
+
+  // go back to previous step
+  prevStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step - 1 });
+  }
+
+  // proceed to the next step
+  nextStep = () => {
+    const { step } = this.state;
+    this.setState({ step: step + 1 });
+  }
+
+  // handle field change
+  handleChange = input => e => {
+    this.setState({ [input]: e.target.value });
+  }
+
   render() {
-    return (
-      <div>
-        Hello
-      </div>
-    )
+    const { step } = this.state;
+    const { firstName, lastName, email, occupation, city, bio } = this.state;
+    const values = { firstName, lastName, email, occupation, city, bio };
+
+    switch (step) {
+      case 1:
+        return (
+          <FormUserDetails
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 2:
+        return (
+          <FormPersonnalDetails
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            handleChange={this.handleChange}
+            values={values}
+          />
+        );
+      case 3:
+        return (
+          <Confirm
+            nextStep={this.nextStep}
+            prevStep={this.prevStep}
+            values={values}
+          />
+        );
+      case 4:
+        return <Success />;
+      default:
+        (console.log('This is a multi-step form built with React.'))
+    }
   }
 }
