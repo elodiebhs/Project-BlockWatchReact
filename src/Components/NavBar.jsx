06496be8fbdae2c0f7cd894a4ed63db15df7f4bd
elodiebhs <elodiebouthors@hotmail.com>
2021-09-27 11:54:21 -0700

    change font

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,9 +8,9 @@ function Navbar () {
     <section className="navbar">
       <a href="/" className="navbar-item">Home</a>
       <a href="/about" className="navbar-item">About</a>
-      <a href="/portfolio" className="navbar-item">Portfolio</a>
-      <a href="/shop" className="navbar-item">Shop</a>
-      <a href="/blog" className="navbar-item">Blog</a>
+      <a href="/portfolio" className="navbar-item">Report an Incident</a>
+      <a href="/shop" className="navbar-item">See all Incident</a>
+      <a href="/blog" className="navbar-item">To do</a>
       <a href="/contact" className="navbar-item">Contact</a>
   </section>
   )
